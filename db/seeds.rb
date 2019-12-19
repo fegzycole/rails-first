4d947a9d713beab04e6445f36b9206ef342dc839
fegzycole <fergusoniyara@gmail.com>
2019-12-19 23:17:47 +0100

    Created seed file for data:

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'faker'
+
+skills = %w[Ruby Django NodeJS MongoDB Postgresql]
+
+10.times do |blog|
+  Blog.create!(
+    title: "My blogpost #{blog}",
+    body: Faker::Lorem.paragraph
+  )
+end
+
+puts '10 blogposts created'
+
+5.times do |skill|
+  Skill.create!(
+    title: skills[skill],
+    percent_utilized: 5 * skill
+  )
+end
+
+puts 'five skills created'
+
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio}",
+    subtitle: 'My great service',
+    body: Faker::Lorem.paragraph,
+    main_image: 'https://via.placeholder.com/600x400',
+    thumb_image: 'https://via.placeholder.com/350x200'
+  )
+end
