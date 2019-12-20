0285b274a475cf595a489f73c4054b384817fdad
fegzycole <fergusoniyara@gmail.com>
2019-12-20 08:15:58 +0100

    Add links to show page

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -34,6 +34,10 @@ class PortfoliosController < ApplicationController
     @portfolio = Portfolio.find(params[:id])
   end
 
+  def show
+    @portfolio = Portfolio.find(params[:id])
+  end
+
   private
 
   def portfolio_params
