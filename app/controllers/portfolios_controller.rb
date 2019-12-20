780370ec20667ef5aa5a94cec930da9674ba5aff
fegzycole <fergusoniyara@gmail.com>
2019-12-20 01:40:54 +0100

    Add edit view and edit/update controller methods

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -19,6 +19,21 @@ class PortfoliosController < ApplicationController
     end
   end
 
+  def update
+    @portfolio = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio.update(portfolio_params)
+        format.html { redirect_to portfolios_url, notice: 'Portfolio updated successfully'}
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def edit
+    @portfolio = Portfolio.find(params[:id])
+  end
+
   private
 
   def portfolio_params
